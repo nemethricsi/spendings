1c09e5bb140fe80a3652f5b2e6113a071985cdce
Richárd Németh <nemethricsi@gmail.com>
2021-08-04 20:31:41 +0200

    separate webpack by dev and prod

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -2,14 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-  },
-  devServer: {
-    port: 3000,
   },
   module: {
     rules: [
