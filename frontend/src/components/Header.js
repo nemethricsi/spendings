f9fb229ec0960a8032a99b1119a5a7c7a118c8a1
richard_nemeth <richard.nemeth@futurevending.hu>
2021-08-04 12:21:10 +0200

    style nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SiDjango, SiGithub } from 'react-icons/si';
 
 const HeaderStyles = styled.header`
   margin-bottom: 4rem;
@@ -15,15 +16,42 @@ const HeaderStyles = styled.header`
   a {
     text-decoration: none;
     color: inherit;
+    line-height: 0;
+    transition: all 0.1s;
   }
 
   li {
+    font-weight: 500;
     border-bottom: 2px solid transparent;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    line-height: 1;
+    padding: 12px 16px 8px;
+    border-radius: 8px;
+    transition: all 0.1s;
+
+    svg {
+      margin-top: -1px;
+      width: 20px;
+      height: 20px;
+      transition: all 0.1s;
+    }
   }
 
   li:hover {
-    border-bottom: 2px solid;
+    background-color: #f4f7fd;
+    cursor: pointer;
+
+    svg {
+      color: var(--color-blue);
+    }
   }
+
+  a:hover {
+    color: var(--color-blue);
+  }
+
   @media (max-width: 756px) {
     margin-bottom: 2rem;
   }
@@ -60,18 +88,26 @@ export default function Header() {
           </LogoStyles>
           <NavStyles>
             <ul>
-              <li>
-                <a href='#0'>About</a>
-              </li>
-              <li>
-                <a
-                  href='https://spendings-django.herokuapp.com/api'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  REST Api Admin
-                </a>
-              </li>
+              <a
+                href='https://github.com/nemethricsi/spendings'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <li>
+                  <SiGithub />
+                  GitHub
+                </li>
+              </a>
+              <a
+                href='https://spendings-django.herokuapp.com/api'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <li>
+                  <SiDjango />
+                  REST Api
+                </li>
+              </a>
             </ul>
           </NavStyles>
         </FlexWrapper>
